a9c21ce6553ec12aa8b6c7843c14a135db552faf
Eftey <tanjemahamed305@gmail.com>
2024-02-01 21:49:33 +0600

    1

--- code_forces/pratice/1000/800/266A.java
+++ code_forces/pratice/1000/800/266A.java
@@ -1,7 +1,30 @@
+import java.io.*;
+import java.util.*;
 
-public class 266A {
+/*
+    Problem: A. Stones on the Table
+    1) Check if there's a match (temp vs actual)
+    2) If so then c++ else update temp
+    3) Out c
+ */
+
+public class Main {
     public static void main(String[] args) {
-        Scanner x = new Scanner
+        Scanner in = new Scanner(System.in);
+        PrintWriter pw = new PrintWriter(System.out);
 
+        int t = in.nextInt(), c = 0;
+        String s = in.next();
+
+        char temp = 'x';
+        for (char i : s.toCharArray())
+            if (i == temp) c++;
+            else temp = i;
+        pw.println(c);
+        pw.close();
+    }
+
+    static void debug(Object... obj) {
+        System.err.println(Arrays.deepToString(obj));
     }
 }

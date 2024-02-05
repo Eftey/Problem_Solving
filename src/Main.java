6640a50d97cd0abb30c988fba8bea5530f4d5666
Eftey <tanjemahamed305@gmail.com>
2024-02-06 03:19:49 +0600

    5

--- src/Main.java
+++ src/Main.java
@@ -13,30 +13,6 @@ public class Main {
         pw.close();
     }
 
-    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null) return list2;
-        else if (list2 == null) return list1;
-
-        ListNode listNode = new ListNode(), x = list1, y = list2, head = listNode;
-        //ListNode ptr1 = list1, ptr2 = list2, ptr3 = new ListNode(), head = ptr3;
-        while(x != null && y != null) {
-            if(x.val <= y.val) {
-                listNode.next = x;
-                x = x.next;
-            }
-            else {
-                listNode.next = y;
-                y = y.next;
-            }
-            listNode = listNode.next;
-        }
-        if(x == null)
-            listNode.next = y;
-        if(y == null)
-            listNode.next = x;
-        return head.next;
-    }
-
     static void debug(Object... obj) {
         System.err.println(Arrays.deepToString(obj));
     }

00017e04d3aa985cd5d820432a98a32a7d6ee80b
Eftey <tanjemahamed305@gmail.com>
2024-02-28 17:59:02 +0600

    28

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,25 @@ import java.io.*;
 import java.util.*;
 
 /*
-    Problem: The Cooler Dilemma 2
-    1) If y is not divisible by x, then their division is the result
-    2) Else we have to subtract 1 from the result
+    Problem: Arrival of the General
 */
 
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         PrintWriter pw = new PrintWriter(System.out);
-        int t = in.nextInt();
-        for (int tt = 0; tt < t; tt++) {
-            int x = in.nextInt(), y = in.nextInt();
-            pw.println(y % x != 0 ? (int) (y / x) : (int) (y / x) - 1);
+        int n = in.nextInt(), max = 0, min = 0;
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > arr[max]) max = i;
+            if (arr[i] <= arr[min]) min = i;
         }
+        if (max < min) pw.println(max + (n - min - 1));
+        else pw.println(max + (n - min - 2));
         pw.close();
     }
+
 
     static void debug(Object... obj) {
         System.err.println(Arrays.deepToString(obj));

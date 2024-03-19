805ac720ed3d258a4a0761d67f9a53fe51c65e74
Eftey <tanjemahamed305@gmail.com>
2024-03-19 15:27:37 +0600

    19

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 /*
-    Problem: I_love_%username%
+    Problem: C
  */
 
 public class Main {
@@ -10,19 +10,45 @@ public class Main {
         Scanner in = new Scanner(System.in);
         PrintWriter pw = new PrintWriter(System.out);
 
-        int t = in.nextInt(), x = in.nextInt(), xx = 0, c = 0;
-        xx = x;
-        for (int tt = 0; tt < t - 1; tt++) {
-            int ax = in.nextInt();
-            if (ax > x) {
-                c++;
-                x = ax;
-            } else if (ax < xx) {
-                c++;
-                xx = ax;
+        int t = in.nextInt();
+        for (int tt = 0; tt < t; tt++) {
+            int n = in.nextInt();
+            String s = in.next();
+
+            int[] x = new int[2], xx = new int[2];
+            for (int i = 0; i < n; i++) {
+                if (s.charAt(i) == '0') xx[0]++;
+                else xx[1]++;
             }
+
+            int c = Integer.MAX_VALUE, idx = -1;
+            if ((n / 2) + (n % 2) <= xx[1]) {
+                c = n;
+                idx = 0;
+            }
+
+            for (int i = 0; i < n; i++) {
+                if (s.charAt(i) == '0') {
+                    xx[0]--;
+                    x[0]++;
+                } else {
+                    xx[1]--;
+                    x[1]++;
+                }
+
+                int xc = x[0] + x[1], xxc = xx[0] + xx[1];
+                if ((xc / 2) + (xc % 2) <= x[0] && (xxc / 2) + (xxc % 2) <= xx[1]) {
+                    int d = Math.abs(xxc - xc);
+                    if (d < c) {
+                        c = d;
+                        idx = i + 1;
+                    }
+                }
+            }
+
+            pw.println(idx);
         }
-        pw.println(c);
+
         pw.close();
     }
 

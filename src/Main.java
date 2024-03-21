862e3c82b2260353eeac470a9d768adf05b5831b
Eftey <tanjemahamed305@gmail.com>
2024-03-21 23:55:16 +0600

    21

--- src/Main.java
+++ src/Main.java
@@ -9,45 +9,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         PrintWriter pw = new PrintWriter(System.out);
-
-        int t = in.nextInt();
-        for (int tt = 0; tt < t; tt++) {
-            int n = in.nextInt();
-            String s = in.next();
-
-            int[] x = new int[2], xx = new int[2];
-            for (int i = 0; i < n; i++) {
-                if (s.charAt(i) == '0') xx[0]++;
-                else xx[1]++;
-            }
-
-            int c = Integer.MAX_VALUE, idx = -1;
-            if ((n / 2) + (n % 2) <= xx[1]) {
-                c = n;
-                idx = 0;
-            }
-
-            for (int i = 0; i < n; i++) {
-                if (s.charAt(i) == '0') {
-                    xx[0]--;
-                    x[0]++;
-                } else {
-                    xx[1]--;
-                    x[1]++;
-                }
-
-                int xc = x[0] + x[1], xxc = xx[0] + xx[1];
-                if ((xc / 2) + (xc % 2) <= x[0] && (xxc / 2) + (xxc % 2) <= xx[1]) {
-                    int d = Math.abs(xxc - xc);
-                    if (d < c) {
-                        c = d;
-                        idx = i + 1;
-                    }
-                }
-            }
-
-            pw.println(idx);
-        }
+        int n = in.nextInt(), k = in.nextInt(), l = in.nextInt(), c = in.nextInt(), d = in.nextInt(), p = in.nextInt(), nl = in.nextInt(), np = in.nextInt();
+        pw.println(Math.min((int) ((k * l) / nl), Math.min((int) (p / np), c * d)) / n);
 
         pw.close();
     }
